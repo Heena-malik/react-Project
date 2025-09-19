cf25282d5cc0d3b05517d241b1bbd564db002ed2
Heena-malik <heenaamalik1108@gmail.com>
2025-09-19 12:37:50 +0530

    Initial commit

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import barger from "../images/barger.jpeg";
+import sandwich from "../images/sandwich.jpeg";
+import pizza from "../images/pizza.jpeg";
+import hotdog from "../images/hotdog.jpeg";
+import patatoFrenchfries from "../images/patatoFrenchfries.jpeg";
+import pasta from "../images/pasta.jpeg";
+
+export const MenuList = [
+  {
+    name: "barger",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: barger,
+    price: 200,
+  },
+  {
+    name: "sandwich",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: sandwich,
+    price: 250,
+  },
+  {
+    name: "pizza",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: pizza,
+    price: 300,
+  },
+  {
+    name: "hotdog",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: hotdog,
+    price: 100,
+  },
+  {
+    name: "patatoFrenchfries",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: patatoFrenchfries,
+    price: 400,
+  },
+  {
+    name: "pasta",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: pasta,
+    price: 500,
+  },
+];
